fccbb5a5fd05a9b6b9d63247f44e8f2b0e1b2bb1
Claudio Sangeroki <csangeroki@gmail.com>
2020-04-26 22:45:32 -0700

    added icons

--- mengambilPage/app.js
+++ mengambilPage/app.js
@@ -1,13 +1,16 @@
-const main = () => {
-  var newIcon = document.createElement("img");
-  newIcon.src = "../img/bucket.png";
-  newIcon.className = "icon";
-  var iconList = document.getElementById("iconList");
+const injectIcon = (DictObj) => {
+  if DictObj("clue") == true{
+    var newIcon = document.createElement("img");
+    newIcon.src = "../img/bucket.png";
+    newIcon.className = "icon";
+    var iconList = document.getElementById("iconList");
+  
+    iconList.appendChild(newIcon);
+  }else if (DictObj("bascom2"))
 
-  iconList.appendChild(newIcon);
 };
 
-const letsgo2 = async () => {
+const tokenFunc = async () => {
   const response = await fetch("http://sebuahbas.com/api/newtoken", {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
     headers: {
@@ -16,8 +19,81 @@ const letsgo2 = async () => {
   });
 
   const respJson = await response.json();
+
   console.log(respJson);
   if (respJson.status === "SUCCESS") {
     localStorage.setItem("sebuahbascomtoken", respJson.token);
   }
 };
+
+const tempFunc = () => {
+  if (
+    localStorage.getItem("sebuahbascomtoken") === null ||
+    localStorage.length === 0
+  ) {
+    tokenFunc();
+  } else {
+    verification();
+  }
+};
+
+const verification = async () => {
+  const response = await fetch("http://sebuahbas.com/api/permission", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("sebuahbascomtoken"),
+      id: 0,
+    }),
+  });
+
+  const respJson = await response.json();
+  console.log(respJson);
+  if (respJson.status === "SUCCESS") {
+  } else if (respJson.status === "ERROR") {
+    tokenFunc();
+  }
+};
+
+const loadInven = async () => {
+  const response = await fetch("http://sebuahbas.com/api/inventory", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("sebuahbascomtoken"),
+      id: 0,
+    }),
+  });
+
+  const respJson = await response.json();
+  console.log(respJson);
+
+  if (respJson.status === "SUCCESS") {
+    localStorage.setItem("sebuahbascomtoken", respJson.token);
+  }
+};
+
+const setTrue = async () => {
+  const response = await fetch("http://sebuahbas.com/api/inventory/bascom", {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("sebuahbascomtoken"),
+      id: 0,
+    }),
+  });
+
+  const respJson = await response.json();
+  console.log(respJson);
+
+  if (respJson.status === "SUCCESS") {
+    localStorage.setItem("sebuahbascomtoken", respJson.token);
+  }
+};
+
